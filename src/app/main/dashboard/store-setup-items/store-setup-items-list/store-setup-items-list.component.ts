f9b52933e46d6f6cf48fa0520bd812bf631b7525
anup.singh@luezoid.com <anup.singh@luezoid.com>
2020-06-05 17:21:34 +0530

    store-setup-items-list: add filter and search

--- src/app/main/dashboard/store-setup-items/store-setup-items-list/store-setup-items-list.component.ts
+++ src/app/main/dashboard/store-setup-items/store-setup-items-list/store-setup-items-list.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
-import {FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup} from '@angular/forms';
 import {fuseAnimations} from "../../../../../@fuse/animations";
 import {StoreSetupItemsCreateComponent} from '../store-setup-items-create/store-setup-items-create.component';
 import {StoreSetupItemsService} from "../../../../shared/services/store-setup-items.service";
+import {CategoriesListSelectComponent} from '../categories-list-select/categories-list-select.component';
 
 @Component({
     selector: 'app-store-setup-items-list',
@@ -16,17 +17,27 @@ export class StoreSetupItemsListComponent implements OnInit {
     items = [];
     displayedColumns = ['sno', 'id', 'description', 'unit', 'category', 'actions'];
     dialogRef: any;
-
+    itemsFilterForm: FormGroup;
+    categories = [];
     constructor(private storeSetupItemsService: StoreSetupItemsService,
+                private fb: FormBuilder,
                 private _matDialog: MatDialog) {
     }
 
     ngOnInit(): void {
+        this. refresh();
         this.getStores();
     }
+    refresh() {
+        this.itemsFilterForm = this.fb.group({
+            categoryId: [''],
+            search: [''],
+        });
+    }
 
-    getStores() {
-        this.storeSetupItemsService.getStoreSetupItems({'page': -1}).subscribe(data => {
+    getStores(params = {}) {
+        // params['page'] = -1;
+        this.storeSetupItemsService.getStoreSetupItems(params).subscribe(data => {
             this.items = data.items;
             if (this.items && this.items.length > 0) {
                 let i = 1;
@@ -43,7 +54,7 @@ export class StoreSetupItemsListComponent implements OnInit {
             if (data) {
                 this.getStores();
             }
-        })
+        });
     }
 
     editStore(store) {
@@ -56,6 +67,30 @@ export class StoreSetupItemsListComponent implements OnInit {
                 return;
             }
             this.getStores();
+        });
+    }
+    getItems(params) {
+        this.getStores(params);
+    }
+
+    categorySelect() {
+        this.dialogRef = this._matDialog.open(CategoriesListSelectComponent, {
+            panelClass: 'contact-form-dialog',
+        });
+        this.dialogRef.afterClosed().subscribe((response) => {
+            if (!response) {
+                return;
+            }
+            this.categories = [{
+                'name': response.name,
+                'id': response.id
+            }];
+            this.itemsFilterForm.patchValue({
+                categoryId: response.id,
+            });
+            this.getStores({
+                categoryId: response.id,
+            });
         });
     }
 }
