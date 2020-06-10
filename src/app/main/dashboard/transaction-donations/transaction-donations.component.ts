ce5096c0e82cc919b305326fd1deb108411db077
Abhishek <abhishek.v@luezoid.com>
2020-06-10 11:12:07 +0530

    Transaction : DOnation done

--- src/app/main/dashboard/transaction-donations/transaction-donations.component.ts
+++ src/app/main/dashboard/transaction-donations/transaction-donations.component.ts
@@ -3,6 +3,9 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 import {fuseAnimations} from "../../../../@fuse/animations";
 import {AlertService} from "../../../shared/services/alert.service";
 import {TransactionService} from "../../../shared/services/transaction.service";
+import {StoreSetupItemsService} from "../../../shared/services/store-setup-items.service";
+import {StoreSetupStoresService} from "../../../shared/services/store-setup-stores.service";
+import {StoreSetupUnitOfMeasuresService} from "../../../shared/services/store-setup-unit-of-measures.service";
 
 @Component({
     selector: 'app-transaction-donations',
@@ -15,21 +18,32 @@ export class TransactionDonationsComponent implements OnInit {
     donationsForm: FormGroup;
     itemsArr = [];
     companies = [];
+    stores = [];
+    storeItems = [];
+    unitOfMeasuresData = [];
 
-    constructor(private fb: FormBuilder, private alertService: AlertService, private transactionService: TransactionService) {
+    constructor(private fb: FormBuilder,
+                private alertService: AlertService,
+                private transactionService: TransactionService,
+                private storeSetupItemsService: StoreSetupItemsService,
+                private storeSetupStoresService: StoreSetupStoresService,
+                private storeSetupUnitOfMeasuresService: StoreSetupUnitOfMeasuresService) {
     }
 
     ngOnInit(): void {
         this.refresh();
         this.getCompanies();
+        this.getStores();
+        this.getStoreItems();
+        this.getStoreSetupUnitOfMeasure();
     }
 
     refresh() {
         this.donationsForm = this.fb.group({
             'supplierId': [''],
             'storeId': [''],
-            'supplierAddress': [''],
-            'storeName': [''],
+            'supplierAddress': [{value: '', disabled: true}],
+            'storeName': [{value: '', disabled: true}],
             'details': [''],
             'srDocRefNo': [''],
             'date': [''],
@@ -41,9 +55,9 @@ export class TransactionDonationsComponent implements OnInit {
             'unitCost': [''],
             'quantitySysLessPhy': [''],
             'unitCostOfDiff': [''],
-            'totalValuesInWords': [''],
-            'subTotal': [''],
-            'total': ['']
+            'totalValuesInWords': [{value: '', disabled: true}],
+            'subTotal': [{value: '', disabled: true}],
+            'total': [{value: '', disabled: true}]
         });
     }
 
@@ -57,10 +71,19 @@ export class TransactionDonationsComponent implements OnInit {
             });
         }
         if (!repeatItemFound) {
+            let unitOfMeasureName = '';
+            if (this.unitOfMeasuresData && this.unitOfMeasuresData.length > 0) {
+                this.unitOfMeasuresData.forEach(unitOfMeasure => {
+                    if (parseInt(unitOfMeasure.id) === parseInt(unitOfMeasures)) {
+                        unitOfMeasureName = unitOfMeasure.name
+                    }
+                });
+            }
             this.itemsArr.push({
                 'id': itemId,
                 'description': description,
                 'unitOfMeasures': unitOfMeasures,
+                'unitOfMeasureName': unitOfMeasureName,
                 'quantity': quantity,
                 'unitCost': unitCost,
                 'value': parseInt(quantity) * parseInt(unitCost)
@@ -72,20 +95,103 @@ export class TransactionDonationsComponent implements OnInit {
                 'quantity': '',
                 'unitCost': ''
             });
+            this.setTotals();
         } else {
             this.alertService.showErrors('Item already added');
         }
-        console.log('this.itemsArr', this.itemsArr);
+    }
+
+    getStores() {
+        this.stores = [];
+        this.storeSetupStoresService.getStoreSetupStores({page: '-1'}).subscribe(data => {
+            this.stores = data.items;
+        });
+    }
+
+    getStoreItems() {
+        this.storeSetupItemsService.getStoreSetupItems({}).subscribe(data => {
+            this.storeItems = data.items;
+        });
+    }
+
+    getStoreSetupUnitOfMeasure() {
+        this.storeSetupUnitOfMeasuresService.getStoreSetupUnitOfMeasures({'page': -1}).subscribe(data => {
+            this.unitOfMeasuresData = data.items;
+        });
     }
 
     deleteItem(index) {
         this.itemsArr.splice(index, 1);
+        this.setTotals();
     }
 
     getCompanies() {
         this.transactionService.getCompanies({'page': -1}).subscribe(data => {
             this.companies = data.items;
-            console.log('this.companies', this.companies);
         });
+    }
+
+    setSupplierAddress(compId) {
+        let selectedSupplierAddress = '';
+        if (this.companies && this.companies.length > 0) {
+            this.companies.forEach(company => {
+                if (parseInt(company.id) === parseInt(compId)) {
+                    selectedSupplierAddress = company.name
+                }
+            });
+        }
+        this.donationsForm.patchValue({
+            'supplierAddress': selectedSupplierAddress
+        });
+    }
+
+    setStoreName(storeId) {
+        let selectedStoreName = '';
+        if (this.stores && this.stores.length > 0) {
+            this.stores.forEach(store => {
+                if (parseInt(store.id) === parseInt(storeId)) {
+                    selectedStoreName = store.name
+                }
+            });
+        }
+        this.donationsForm.patchValue({
+            'storeName': selectedStoreName
+        });
+    }
+
+    setTotals() {
+        if (this.itemsArr && this.itemsArr.length > 0) {
+            let subTotal = 0;
+            this.itemsArr.forEach(item => {
+                subTotal = subTotal + (parseInt(item.unitCost) * parseInt(item.quantity));
+            });
+            this.donationsForm.patchValue({
+                'subTotal': subTotal,
+                'total': subTotal
+            });
+        } else {
+            this.donationsForm.patchValue({
+                'subTotal': 0,
+                'total': 0
+            });
+        }
+    }
+
+    saveDonation() {
+        this.donationsForm.value['items'] = this.itemsArr;
+        delete this.donationsForm.value['description'];
+        delete this.donationsForm.value['itemId'];
+        delete this.donationsForm.value['quantity'];
+        delete this.donationsForm.value['unitCost'];
+        delete this.donationsForm.value['unitOfMeasures'];
+        delete this.donationsForm.value['unitCostOfDiff'];
+        delete this.donationsForm.value['value'];
+        this.donationsForm.value['date'] = this.donationsForm.value['date'].format('YYYY-MM-DD');
+        this.donationsForm.value['storeName'] = this.donationsForm['controls']['storeName'].value;
+        this.donationsForm.value['totalValuesInWords'] = this.donationsForm['controls']['totalValuesInWords'].value;
+        this.donationsForm.value['subTotal'] = this.donationsForm['controls']['subTotal'].value;
+        this.donationsForm.value['total'] = this.donationsForm['controls']['total'].value;
+        this.donationsForm.value['supplierAddress'] = this.donationsForm['controls']['supplierAddress'].value;
+        console.log('this.donationsForm', this.donationsForm.value);
     }
 }
