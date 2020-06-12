d77d61089d6921bbd493581a93fd967207c74527
Abhishek <abhishek.v@luezoid.com>
2020-06-12 22:38:32 +0530

    Companies mater : minor change

--- src/app/main/dashboard/companies/companies-create/companies-create.component.ts
+++ src/app/main/dashboard/companies/companies-create/companies-create.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {Component, OnInit, Inject, ViewEncapsulation} from '@angular/core';
+import {MatDialogRef, MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {fuseAnimations} from "../../../../../@fuse/animations";
+
 @Component({
-  selector: 'app-companies-create',
-  templateUrl: './companies-create.component.html',
-  styleUrls: ['./companies-create.component.scss']
+    selector: 'app-companies-create',
+    templateUrl: './companies-create.component.html',
+    styleUrls: ['./companies-create.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 export class CompaniesCreateComponent implements OnInit {
-
-  action: any;
+    action: any;
     dialogTitle: any;
     itemForm: FormGroup;
     isSubmitted = false;
@@ -16,55 +19,55 @@ export class CompaniesCreateComponent implements OnInit {
     updateData: any;
     dialogRef: any;
 
-  constructor(public matDialogRef: MatDialogRef<CompaniesCreateComponent>,
-    @Inject(MAT_DIALOG_DATA) private _data: any,
-    private fb: FormBuilder,
-    private _matDialog: MatDialog,
+    constructor(public matDialogRef: MatDialogRef<CompaniesCreateComponent>,
+                @Inject(MAT_DIALOG_DATA) private _data: any,
+                private fb: FormBuilder,
+                private _matDialog: MatDialog,
     ) {
-      this.action = _data.action;
-      if (this.action === 'EDIT') {
-      this.dialogTitle = 'Edit Item';
-      if (_data.store) {
-          this.updateData = _data;
-      }
-      } else {
-      this.dialogTitle = 'Add New Company';
-      }
-}
+        this.action = _data.action;
+        if (this.action === 'EDIT') {
+            this.dialogTitle = 'Edit Item';
+            if (_data.store) {
+                this.updateData = _data;
+            }
+        } else {
+            this.dialogTitle = 'Add New Company';
+        }
+    }
 
-  ngOnInit(): void {
-    this.refresh();
-  }
+    ngOnInit(): void {
+        this.refresh();
+    }
 
-  refresh() {
-    this.itemForm = this.fb.group({
-        'companyName': ['', Validators.required],
-        'city': ['', Validators.required],
-        'address': ['',Validators.required],
-        'state':['',Validators.required],
-        'country':['',Validators.required],
-        'phone':['',Validators.required],
-        'email':['',Validators.required],
-        'website':['',Validators.required],
-        'isActive':[false,Validators.required],
-        'isSupplier':[false,Validators.required],
-        'isCustomer':[false,Validators.required],
-        'isCashbook':[false,Validators.required],
-        'isOneoff':[false,Validators.required],
-        'isPF':[false,Validators.required]
-        
-        // 'categoryId': ['', Validators.required],
-        // 'measurementId': ['', Validators.required],
-        // 'unitPrice': ['', Validators.required],
-        // 'leadDays': ['', Validators.required],
-        // 'reorderQuantity': ['', Validators.required],
-        // 'salesCommission': ['', Validators.required],
-        // 'minimumQuantity': ['', Validators.required],
-        // 'maximumQuantity': ['', Validators.required],
-        // 'isChargedVat': [false, Validators.required],
-        // 'isChargedOtherTax': [false, Validators.required],
-        // 'isPhysicalQuantity': [false, Validators.required]
-    });
-}
+    refresh() {
+        this.itemForm = this.fb.group({
+            'companyName': ['', Validators.required],
+            'city': ['', Validators.required],
+            'address': ['', Validators.required],
+            'state': ['', Validators.required],
+            'country': ['', Validators.required],
+            'phone': ['', Validators.required],
+            'email': ['', Validators.required],
+            'website': ['', Validators.required],
+            'isActive': [false, Validators.required],
+            'isSupplier': [false, Validators.required],
+            'isCustomer': [false, Validators.required],
+            'isCashbook': [false, Validators.required],
+            'isOneoff': [false, Validators.required],
+            'isPF': [false, Validators.required]
+
+            // 'categoryId': ['', Validators.required],
+            // 'measurementId': ['', Validators.required],
+            // 'unitPrice': ['', Validators.required],
+            // 'leadDays': ['', Validators.required],
+            // 'reorderQuantity': ['', Validators.required],
+            // 'salesCommission': ['', Validators.required],
+            // 'minimumQuantity': ['', Validators.required],
+            // 'maximumQuantity': ['', Validators.required],
+            // 'isChargedVat': [false, Validators.required],
+            // 'isChargedOtherTax': [false, Validators.required],
+            // 'isPhysicalQuantity': [false, Validators.required]
+        });
+    }
 
 }
--- src/app/main/dashboard/companies/companies-list/companies-list.component.ts
+++ src/app/main/dashboard/companies/companies-list/companies-list.component.ts
@@ -1,42 +1,47 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {fuseAnimations} from '../../../../../@fuse/animations';
-export interface master
-{
-  sno:number;
-  companyId:string;
-  companyName:string;
-  companyAddress:string;
+
+export interface master {
+    sno: number;
+    companyId: string;
+    companyName: string;
+    companyAddress: string;
 }
 
-const MASTERS_DATA: master[]=[
-  {sno:1,companyId:'SPP0957',companyName:'ABBA',companyAddress:'Kano Nigeria'},
-  {sno:2,companyId:'2018-926B',companyName:'ABDULLAHI',companyAddress:'Dept of Agriculture eng.'},
-  {sno:3,companyId:'2019-5531',companyName:'A.B.M.',companyAddress:'NYSC-Kano'},
+const MASTERS_DATA: master[] = [
+    {sno: 1, companyId: 'SPP0957', companyName: 'ABBA', companyAddress: 'Kano Nigeria'},
+    {sno: 2, companyId: '2018-926B', companyName: 'ABDULLAHI', companyAddress: 'Dept of Agriculture eng.'},
+    {sno: 3, companyId: '2019-5531', companyName: 'A.B.M.', companyAddress: 'NYSC-Kano'},
 ];
+
 @Component({
-  selector: 'app-companies-list',
-  templateUrl: './companies-list.component.html',
-  styleUrls: ['./companies-list.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations
+    selector: 'app-companies-list',
+    templateUrl: './companies-list.component.html',
+    styleUrls: ['./companies-list.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 export class CompaniesListComponent implements OnInit {
 
-  constructor() { }
-  displayedColumns:string[]=['sno','companyId','companyName','companyAddress','actions'];
-  items=MASTERS_DATA;
-  ngOnInit(): void {
-   // console.log(this.dataSource)
-  }
-  editStore(item)
-  {
+    constructor() {
+    }
 
-  }
-  deleteItemModal(item)
-  {
+    displayedColumns: string[] = ['sno', 'companyId', 'companyName', 'companyAddress', 'actions'];
+    items = MASTERS_DATA;
 
-  }
-  onPageChange(page) {
-  }
-  
+    ngOnInit(): void {
+        // console.log(this.dataSource)
+    }
+
+    editStore(item) {
+
+    }
+
+    deleteItemModal(item) {
+
+    }
+
+    onPageChange(page) {
+    }
+
 }
