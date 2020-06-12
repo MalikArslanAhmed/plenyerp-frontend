fd30d2b5d928004747e8675a9635f3ff2397eabd
Manisha97 <manisha.srivastava@vaamoz.com>
2020-06-12 20:11:44 +0530

    companies masters view done

--- src/app/main/dashboard/companies/companies-create/companies-create.component.ts
+++ src/app/main/dashboard/companies/companies-create/companies-create.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 @Component({
   selector: 'app-companies-create',
   templateUrl: './companies-create.component.html',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompaniesCreateComponent implements OnInit {
 
-  constructor() { }
+  action: any;
+    dialogTitle: any;
+    itemForm: FormGroup;
+    isSubmitted = false;
+    qualifications: any = [];
+    updateData: any;
+    dialogRef: any;
+
+  constructor(public matDialogRef: MatDialogRef<CompaniesCreateComponent>,
+    @Inject(MAT_DIALOG_DATA) private _data: any,
+    private fb: FormBuilder,
+    private _matDialog: MatDialog,
+    ) {
+      this.action = _data.action;
+      if (this.action === 'EDIT') {
+      this.dialogTitle = 'Edit Item';
+      if (_data.store) {
+          this.updateData = _data;
+      }
+      } else {
+      this.dialogTitle = 'Add New Company';
+      }
+}
 
   ngOnInit(): void {
+    this.refresh();
   }
+
+  refresh() {
+    this.itemForm = this.fb.group({
+        'companyName': ['', Validators.required],
+        'city': ['', Validators.required],
+        'address': ['',Validators.required],
+        'state':['',Validators.required],
+        'country':['',Validators.required],
+        'phone':['',Validators.required],
+        'email':['',Validators.required],
+        'website':['',Validators.required],
+        'isActive':[false,Validators.required],
+        'isSupplier':[false,Validators.required],
+        'isCustomer':[false,Validators.required],
+        'isCashbook':[false,Validators.required],
+        'isOneoff':[false,Validators.required],
+        'isPF':[false,Validators.required]
+        
+        // 'categoryId': ['', Validators.required],
+        // 'measurementId': ['', Validators.required],
+        // 'unitPrice': ['', Validators.required],
+        // 'leadDays': ['', Validators.required],
+        // 'reorderQuantity': ['', Validators.required],
+        // 'salesCommission': ['', Validators.required],
+        // 'minimumQuantity': ['', Validators.required],
+        // 'maximumQuantity': ['', Validators.required],
+        // 'isChargedVat': [false, Validators.required],
+        // 'isChargedOtherTax': [false, Validators.required],
+        // 'isPhysicalQuantity': [false, Validators.required]
+    });
+}
 
 }
--- src/app/main/dashboard/companies/companies.component.ts
+++ src/app/main/dashboard/companies/companies.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FuseSidebarService } from '@fuse/components/sidebar/sidebar.service';
+import { MatDialog } from '@angular/material/dialog';
+import { FormGroup } from '@angular/forms';
+import { CompaniesCreateComponent } from './companies-create/companies-create.component';
 
 @Component({
   selector: 'app-companies',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./companies.component.scss']
 })
 export class CompaniesComponent implements OnInit {
-
-  constructor() { }
+  dialogRef: any;
+  constructor(private _fuseSidebarService:FuseSidebarService,
+    private _matDialog:MatDialog) { }
 
   ngOnInit(): void {
+  }
+  
+  addCompany()
+  {
+    this.dialogRef = this._matDialog.open(CompaniesCreateComponent, {
+      panelClass: 'contact-form-dialog',
+      data: {action: 'CREATE'}
+  });
+  this.dialogRef.afterClosed().subscribe((response: FormGroup) => {
+      if (!response) {
+          return;
+      }
+      //this.getStores.getStores();
+  });
   }
 
 }
