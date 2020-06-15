fa4d566b3d97b8a86e6f8bced43cc232b325030f
Nirmal.bohra <nirmal.bohra@luezoid.com>
2020-06-15 18:27:03 +0530

    report bin card view done

--- src/app/main/dashboard/report-bin-card/report-bin-card.component.ts
+++ src/app/main/dashboard/report-bin-card/report-bin-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-report-bin-card',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-bin-card.component.scss']
 })
 export class ReportBinCardComponent implements OnInit {
+    binCardForm: FormGroup;
+    itemsArr = [
+        {date: '12/06/2020', description: 'lorem ipsum', in: 12, unitCost: '1221', out: 6, balance: 6},
+        {date: '13/06/2020', description: 'lorem ipsum2', in: 12, unitCost: '2221', out: 8, balance: 4}
+    ];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+      this.refresh();
   }
+
+    refresh() {
+        this.binCardForm = this.fb.group({
+            openingDate: [''],
+            closingDate: [''],
+            itemId: [''],
+            itemName: [''],
+            store: ['']
+        });
+    }
 
 }
