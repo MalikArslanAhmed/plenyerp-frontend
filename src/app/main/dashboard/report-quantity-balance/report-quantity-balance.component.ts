0c9e8b4227e8f67ebead1a4ba1f72d55bf7a3de9
Nirmal.bohra <nirmal.bohra@luezoid.com>
2020-06-15 18:52:21 +0530

    report quantity balance view done

--- src/app/main/dashboard/report-quantity-balance/report-quantity-balance.component.ts
+++ src/app/main/dashboard/report-quantity-balance/report-quantity-balance.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-report-quantity-balance',
-  templateUrl: './report-quantity-balance.component.html',
-  styleUrls: ['./report-quantity-balance.component.scss']
+    selector: 'app-report-quantity-balance',
+    templateUrl: './report-quantity-balance.component.html',
+    styleUrls: ['./report-quantity-balance.component.scss']
 })
 export class ReportQuantityBalanceComponent implements OnInit {
+    binCardForm: FormGroup;
+    itemsArr = [
+        {itemCode: 'ab123', description: 'lorem ipsum', periodBalance: 12, stockValue: '1221', unitOfMeasure: 'kg'},
+        {itemCode: 'jk123', description: 'lorem ipsum2', periodBalance: 5, stockValue: '221', unitOfMeasure: 'kg'}
+    ];
 
-  constructor() { }
+    constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.refresh();
+    }
+
+    refresh() {
+        this.binCardForm = this.fb.group({
+            openingDate: [''],
+            closingDate: [''],
+            store: [''],
+            closingMethod: ['']
+        });
+    }
 
 }
