7cd28f0014e49ed628265a5bd925de21e4a978c4
Abhishek <abhishek.v@luezoid.com>
2020-10-16 12:34:16 +0530

    WIP : Rport changes

--- src/app/main/dashboard/trial-balance/trial-balance-list/trial-balance-list.component.ts
+++ src/app/main/dashboard/trial-balance/trial-balance-list/trial-balance-list.component.ts
@@ -1,73 +1,100 @@
 import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { TrialBalanceReportService } from 'app/shared/services/trial-balance-report.service';
 import {fuseAnimations} from "../../../../../@fuse/animations";
 
 @Component({
-  selector: 'app-trial-balance-list',
-  templateUrl: './trial-balance-list.component.html',
-  styleUrls: ['./trial-balance-list.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations
+    selector: 'app-trial-balance-list',
+    templateUrl: './trial-balance-list.component.html',
+    styleUrls: ['./trial-balance-list.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 export class TrialBalanceListComponent implements OnInit {
 
-  dataSource =[
-    {
-      'sno':1,
-      'accTitle':'Ashish',
-      'title' : 'Ashish1',
-      'credit' : 1000,
-      'debit': 2000,
-      'balance':1000,
-      'reportDetails':[
-        {
-          'fullCode':1,
-          'lineCode':2,
-          'title':'Govt share of FAAC',
-          'credit': 5000,
-          'debit': 2000,
-          'balance': 0.00
-        },
-        {
-          'fullCode':2,
-          'lineCode':2,
-          'title':'Grants',
-          'credit': 5000,
-          'debit': 2000,
-          'balance': 0.00
-        }
-      ]
-    },
-    {
-      'sno':2,
-      'accTitle':'Mishra',
-      'title' : 'Ashish2',
-      'credit' : 1000,
-      'debit': 2000,
-      'balance':1000,
-      'reportDetails':[
-        {
-          'fullCode':1,
-          'lineCode':2,
-          'title':'Govt share of FAAC',
-          'credit': 5000,
-          'debit': 2000,
-          'balance': 0.00
-        },
-        {
-          'fullCode':2,
-          'lineCode':2,
-          'title':'Grants',
-          'credit': 5000,
-          'debit': 2000,
-          'balance': 0.00
-        }
-      ]
+    trailReportMainData =[
+        // {
+        //   'sno':1,
+        //   'accTitle':'Ashish',
+        //   'title' : 'Ashish1',
+        //   'credit' : 1000,
+        //   'debit': 2000,
+        //   'balance':1000,
+        //   'reportDetails':[
+        //     {
+        //       'fullCode':1,
+        //       'lineCode':2,
+        //       'title':'Govt share of FAAC',
+        //       'credit': 5000,
+        //       'debit': 2000,
+        //       'balance': 0.00
+        //     },
+        //     {
+        //       'fullCode':2,
+        //       'lineCode':2,
+        //       'title':'Grants',
+        //       'credit': 5000,
+        //       'debit': 2000,
+        //       'balance': 0.00
+        //     }
+        //   ]
+        // },
+        // {
+        //   'sno':2,
+        //   'accTitle':'Mishra',
+        //   'title' : 'Ashish2',
+        //   'credit' : 1000,
+        //   'debit': 2000,
+        //   'balance':1000,
+        //   'reportDetails':[
+        //     {
+        //       'fullCode':1,
+        //       'lineCode':2,
+        //       'title':'Govt share of FAAC111',
+        //       'credit': 5000,
+        //       'debit': 2000,
+        //       'balance': 0.00
+        //     },
+        //     {
+        //       'fullCode':21,
+        //       'lineCode':2,
+        //       'title':'Grants',
+        //       'credit': 5000,
+        //       'debit': 2000,
+        //       'balance': 0.00
+        //     }
+        //   ]
+        // }
+    ];
+
+    childTrialBalanceData = [];
+
+    constructor(private trialBalanceReportService:TrialBalanceReportService) { }
+
+    ngOnInit(): void {
+        //   console.log("abc",this.dataSource)
+        this.getTrailBalanceData();
     }
-  ];
-  constructor() { }
 
-  ngOnInit(): void {
- //   console.log("abc",this.dataSource)
-  }
+    getTrailBalanceData()
+    {
+        this.trialBalanceReportService.getTrailReport({}).subscribe(data=> {
+            this.trailReportMainData = data.items
+        })
+    }
 
+    getChildReport(data)
+    {
+        const params = {};
+        //console.log("data",data.economicSegmentId);
+        if(data && data.economicSegmentId)
+        {
+            //console.log("abc");
+            params['parentId'] = data.economicSegmentId;
+            console.log('apr',params);
+
+            this.trialBalanceReportService.getTrailReport(params).subscribe(data=> {
+                this.childTrialBalanceData = data.items
+            })
+        }
+    }
 }
--- src/app/main/dashboard/trial-balance/trial-balance.component.ts
+++ src/app/main/dashboard/trial-balance/trial-balance.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { fuseAnimations } from '@fuse/animations';
+import { AlertService } from 'app/shared/services/alert.service';
+import { TrialBalanceReportService } from 'app/shared/services/trial-balance-report.service';
+import * as moment from 'moment';
 
 
 @Component({
@@ -11,17 +14,68 @@ import { fuseAnimations } from '@fuse/animations';
     animations: fuseAnimations
 })
 export class TrialBalanceComponent implements OnInit {
-    filterTrialBalanceReportForm: FormGroup;
 
-    constructor(private fb:FormBuilder) {
+    filterTrialBalanceReportForm: FormGroup;
+    trailReportMainData =[];
+    childTrialBalanceData = [];
+
+    constructor(private fb:FormBuilder,
+                private trialBalanceReportService:TrialBalanceReportService,
+                private alertService:AlertService) {
     }
 
     ngOnInit(): void {
         this.filterTrialBalanceReportForm = this.fb.group({
-            'from': [''],
-            'to': ['']
+            'from': ['',Validators.required],
+            'to': ['',Validators.required]
         });
+        this.getTrailBalanceData({});
+    }
 
+    getTrailBalanceData(params)
+    {
+        this.trialBalanceReportService.getTrailReport(params).subscribe(data=> {
+            this.trailReportMainData = data.items
+        })
+    }
+
+    filterTrailBalance()
+    {
+        if(this.filterTrialBalanceReportForm.value.from === '' || this.filterTrialBalanceReportForm.value.to === '')
+        {
+            if(this.filterTrialBalanceReportForm.value.from === '')
+            {
+                this.alertService.showErrors("From date is required");
+            }
+            else if(this.filterTrialBalanceReportForm.value.to === '')
+            {
+                this.alertService.showErrors("To date is required");
+            }
+        }
+        else
+        {
+            const f = this.filterTrialBalanceReportForm.value;
+            const fromDate = moment(f.from).format('YYYY-MM-DD');
+            const toDate = moment(f.to).format('YYYY-MM-DD');
+            const params ={
+                fromDate : fromDate,
+                toDate : toDate
+            }
+            this.getTrailBalanceData(params);
+        }
+
+    }
+
+    getChildReport(data)
+    {
+        const params = {};
+        if(data && data.economicSegmentId)
+        {
+            params['parentId'] = data.economicSegmentId;
+            this.trialBalanceReportService.getTrailReport(params).subscribe(data=> {
+                this.childTrialBalanceData = data.items
+            });
+        }
     }
 
 }
