dfd0a510db8bbbcf6d3ed87cbe7eb8a13f89e65e
ashish.mishra <ashish.mishra@luezoid.com>
2020-10-21 20:11:15 +0530

    jv ledger popup

--- src/app/main/dashboard/journal-voucher-ledger-report/journal-voucher-ledger-report.component.ts
+++ src/app/main/dashboard/journal-voucher-ledger-report/journal-voucher-ledger-report.component.ts
@@ -1,8 +1,11 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import {JournalVoucherLedgerReportService} from 'app/shared/services/journal-voucher-ledger-report.service';
 import {fuseAnimations} from '../../../../@fuse/animations';
 import {AlertService} from "../../../shared/services/alert.service";
+import { EconomicSegmentSelectComponent } from '../journal-voucher/economic-segment-select/economic-segment-select.component';
+import { ProgrammingSegmentSelectComponent } from '../journal-voucher/programming-segment-select/programming-segment-select.component';
 
 @Component({
     selector: 'app-journal-voucher-ledger-report',
@@ -13,7 +16,12 @@ import {AlertService} from "../../../shared/services/alert.service";
 })
 export class JournalVoucherLedgerReportComponent implements OnInit {
     jvReportData: any;
+    dialogRef: any;
     filterJVLegderReportForm: FormGroup;
+    economicSegments = [];
+    programmeSegments = [];
+    economicPopupShow = false;;
+    programmePopupShow = false;
     sourceApp = [
         {
             'id': 1,
@@ -27,13 +35,15 @@ export class JournalVoucherLedgerReportComponent implements OnInit {
 
     constructor(private jvLedgerReportService: JournalVoucherLedgerReportService,
                 private fb: FormBuilder,
-                private alertService: AlertService) {
+                private alertService: AlertService,
+                private _matDialog: MatDialog) {
     }
 
     ngOnInit(): void {
         this.filterJVLegderReportForm = this.fb.group({
             'type': [''],
-            'id': ['']
+            'economicSegmentId': [''],
+            'programmeSegmentId':['']
         });
         this.getLedgerReport({});
     }
@@ -54,11 +64,64 @@ export class JournalVoucherLedgerReportComponent implements OnInit {
         } else {
             const params = {};
             if (this.filterJVLegderReportForm.value.type === 1) {
-                params['economicSegmentId'] = this.filterJVLegderReportForm.value.id
+                
+                params['economicSegmentId'] = this.filterJVLegderReportForm.value.economicSegmentId
             } else if (this.filterJVLegderReportForm.value.type === 2) {
-                params['programmeSegmentId'] = this.filterJVLegderReportForm.value.id
+                params['programmeSegmentId'] = this.filterJVLegderReportForm.value.programmeSegmentId
             }
             this.getLedgerReport(params);
         }
     }
+
+    selectOption(event)
+    {
+        if(event.value === 1)
+        {
+            this.economicPopupShow = true;
+            this.programmePopupShow = false;
+        }
+        if(event.value === 2)
+        {
+            this.programmePopupShow = true;
+            this.economicPopupShow = false;
+        }
+    }
+    economicSegmentSelect() {
+        this.dialogRef = this._matDialog.open(EconomicSegmentSelectComponent, {
+            panelClass: 'contact-form-dialog',
+        });
+        this.dialogRef.afterClosed().subscribe((response) => {
+            if (!response) {
+                return;
+            }
+            this.economicSegments = [{
+                'name': response.name,
+                'id': response.id
+            }];
+            this.filterJVLegderReportForm.patchValue({
+                economicSegmentId: response.id,
+                disabled: true
+            });
+        });
+    }
+
+    programmeSegmentSelect() {
+        this.dialogRef = this._matDialog.open(ProgrammingSegmentSelectComponent, {
+            panelClass: 'contact-form-dialog',
+        });
+        this.dialogRef.afterClosed().subscribe((response) => {
+            if (!response) {
+                return;
+            }
+            this.programmeSegments = [{
+                'name': response.name,
+                'id': response.id
+            }];
+            this.filterJVLegderReportForm.patchValue({
+                programmeSegmentId: response.id,
+                disabled: true
+            });
+        });
+    }
+
 }
