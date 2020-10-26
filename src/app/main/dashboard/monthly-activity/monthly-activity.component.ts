4c767ae49cfcd12ee6ff82a54812eb3a7bb0bfd0
Abhishek <abhishek.v@luezoid.com>
2020-10-26 19:37:59 +0530

    Report : minor changes

--- src/app/main/dashboard/monthly-activity/monthly-activity.component.ts
+++ src/app/main/dashboard/monthly-activity/monthly-activity.component.ts
@@ -1,37 +1,20 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { fuseAnimations } from '@fuse/animations';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {fuseAnimations} from '@fuse/animations';
 
 @Component({
-  selector: 'app-monthly-activity',
-  templateUrl: './monthly-activity.component.html',
-  styleUrls: ['./monthly-activity.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations
+    selector: 'app-monthly-activity',
+    templateUrl: './monthly-activity.component.html',
+    styleUrls: ['./monthly-activity.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 export class MonthlyActivityComponent implements OnInit {
-  panelOpenState = false;
-  data = {
-    "message": "Resource created/Updated",
-    "data": {
-        "revenue": [
-            {
-                "name": "Current Assets",
-                "combinedCode": 31,
-                "january": 0,
-                "february": 0,
-                "march": 0,
-                "april": 0,
-                "may": 100,
-                "june": 0,
-                "july": 0,
-                "august": 200,
-                "september": 0,
-                "november": 0,
-                "december": 0,
-                "previousYears": 0,
-                "total": 300,
-                "child":[
-                  {
+    // panelOpenState = false;
+    data = {
+        "message": "Resource created/Updated",
+        "data": {
+            "revenue": [
+                {
                     "name": "Current Assets",
                     "combinedCode": 31,
                     "january": 0,
@@ -47,42 +30,80 @@ export class MonthlyActivityComponent implements OnInit {
                     "december": 0,
                     "previousYears": 0,
                     "total": 300,
-                    "child":[
-                      {
-                        "name": "Current Assets",
-                        "combinedCode": 31,
-                        "january": 0,
-                        "february": 0,
-                        "march": 0,
-                        "april": 0,
-                        "may": 100,
-                        "june": 0,
-                        "july": 0,
-                        "august": 200,
-                        "september": 0,
-                        "november": 0,
-                        "december": 0,
-                        "previousYears": 0,
-                        "total": 300
-                      },
-                      {
-                        "name": "Current Assets",
-                        "combinedCode": 31,
-                        "january": 0,
-                        "february": 0,
-                        "march": 0,
-                        "april": 0,
-                        "may": 100,
-                        "june": 0,
-                        "july": 0,
-                        "august": 200,
-                        "september": 0,
-                        "november": 0,
-                        "december": 0,
-                        "previousYears": 0,
-                        "total": 300,
-                        "child":[
-                          {
+                    "child": [
+                        {
+                            "name": "Current Assets",
+                            "combinedCode": 31,
+                            "january": 0,
+                            "february": 0,
+                            "march": 0,
+                            "april": 0,
+                            "may": 100,
+                            "june": 0,
+                            "july": 0,
+                            "august": 200,
+                            "september": 0,
+                            "november": 0,
+                            "december": 0,
+                            "previousYears": 0,
+                            "total": 300,
+                            "child": [
+                                {
+                                    "name": "Current Assets",
+                                    "combinedCode": 31,
+                                    "january": 0,
+                                    "february": 0,
+                                    "march": 0,
+                                    "april": 0,
+                                    "may": 100,
+                                    "june": 0,
+                                    "july": 0,
+                                    "august": 200,
+                                    "september": 0,
+                                    "november": 0,
+                                    "december": 0,
+                                    "previousYears": 0,
+                                    "total": 300
+                                },
+                                {
+                                    "name": "Current Assets",
+                                    "combinedCode": 31,
+                                    "january": 0,
+                                    "february": 0,
+                                    "march": 0,
+                                    "april": 0,
+                                    "may": 100,
+                                    "june": 0,
+                                    "july": 0,
+                                    "august": 200,
+                                    "september": 0,
+                                    "november": 0,
+                                    "december": 0,
+                                    "previousYears": 0,
+                                    "total": 300,
+                                    "child": [
+                                        {
+                                            "name": "Current Assets",
+                                            "combinedCode": 31,
+                                            "january": 0,
+                                            "february": 0,
+                                            "march": 0,
+                                            "april": 0,
+                                            "may": 100,
+                                            "june": 0,
+                                            "july": 0,
+                                            "august": 200,
+                                            "september": 0,
+                                            "november": 0,
+                                            "december": 0,
+                                            "previousYears": 0,
+                                            "total": 300
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
                             "name": "Current Assets",
                             "combinedCode": 31,
                             "january": 0,
@@ -98,14 +119,12 @@ export class MonthlyActivityComponent implements OnInit {
                             "december": 0,
                             "previousYears": 0,
                             "total": 300
-                          }
-                        ]
-                      }
+                        }
                     ]
-                  },
-                  {
-                    "name": "Current Assets",
-                    "combinedCode": 31,
+                },
+                {
+                    "name": "Non-Current Assets",
+                    "combinedCode": 32,
                     "january": 0,
                     "february": 0,
                     "march": 0,
@@ -119,33 +138,16 @@ export class MonthlyActivityComponent implements OnInit {
                     "december": 0,
                     "previousYears": 0,
                     "total": 300
-                  }
-                ]
-            },
-            {
-                "name": "Non-Current Assets",
-                "combinedCode": 32,
-                "january": 0,
-                "february": 0,
-                "march": 0,
-                "april": 0,
-                "may": 100,
-                "june": 0,
-                "july": 0,
-                "august": 200,
-                "september": 0,
-                "november": 0,
-                "december": 0,
-                "previousYears": 0,
-                "total": 300
-            }
-        ]
-    }
-}
-  constructor() { }
+                }
+            ]
+        }
+    };
 
-  ngOnInit(): void {
-    //console.log("month",this.data)
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        //console.log("month",this.data)
+    }
 
 }
--- src/app/main/dashboard/statement-of-position/statement-of-position.component.ts
+++ src/app/main/dashboard/statement-of-position/statement-of-position.component.ts
@@ -1,60 +1,60 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { fuseAnimations } from '@fuse/animations';
-import { AlertService } from 'app/shared/services/alert.service';
-import { JournalVoucherLedgerReportService } from 'app/shared/services/journal-voucher-ledger-report.service';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {fuseAnimations} from '@fuse/animations';
+import {AlertService} from 'app/shared/services/alert.service';
+import {JournalVoucherLedgerReportService} from 'app/shared/services/journal-voucher-ledger-report.service';
 import * as moment from 'moment';
 
 @Component({
-  selector: 'app-statement-of-position',
-  templateUrl: './statement-of-position.component.html',
-  styleUrls: ['./statement-of-position.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations
+    selector: 'app-statement-of-position',
+    templateUrl: './statement-of-position.component.html',
+    styleUrls: ['./statement-of-position.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 export class StatementOfPositionComponent implements OnInit {
-  filterStatementOfPositionReportForm: FormGroup;
-  statmentPositionData = [];
-  assetData : any;
-  liabilitiesData : any;
-  constructor(private jvLedgerReportService: JournalVoucherLedgerReportService,
-    private fb: FormBuilder,
-    private alertService: AlertService) { }
+    filterStatementOfPositionReportForm: FormGroup;
+    statmentPositionData = [];
+    assetData: any;
+    liabilitiesData: any;
 
-  ngOnInit(): void {
-    this.filterStatementOfPositionReportForm = this.fb.group({
-      'from': ['', Validators.required],
-      'to': ['', Validators.required]
-    });
-    this.getStatementPositionData({});
-  }
-
-  getStatementPositionData(params)
-  {
-    this.jvLedgerReportService.getStatementPositionReport(params).subscribe(data => {
-      this.statmentPositionData = data;
-      this.assetData = this.statmentPositionData['asset'];
-      this.liabilitiesData = this.statmentPositionData['liabilities'];
-    });
-  }
-
-  filterStatementOfPosition()
-  {
-    if (this.filterStatementOfPositionReportForm.value.from === '' || this.filterStatementOfPositionReportForm.value.to === '') {
-      if (this.filterStatementOfPositionReportForm.value.from === '') {
-          this.alertService.showErrors("From date is required");
-      } else if (this.filterStatementOfPositionReportForm.value.to === '') {
-          this.alertService.showErrors("To date is required");
-      }
-  } else {
-      const f = this.filterStatementOfPositionReportForm.value;
-      const fromDate = moment(f.from).format('YYYY-MM-DD');
-      const toDate = moment(f.to).format('YYYY-MM-DD');
-      const params = {
-          fromDate: fromDate,
-          toDate: toDate
-      };
-      this.getStatementPositionData(params);
+    constructor(private jvLedgerReportService: JournalVoucherLedgerReportService,
+                private fb: FormBuilder,
+                private alertService: AlertService) {
     }
-  }
+
+    ngOnInit(): void {
+        this.filterStatementOfPositionReportForm = this.fb.group({
+            'from': ['', Validators.required],
+            'to': ['', Validators.required]
+        });
+        this.getStatementPositionData({});
+    }
+
+    getStatementPositionData(params) {
+        this.jvLedgerReportService.getStatementPositionReport(params).subscribe(data => {
+            this.statmentPositionData = data;
+            this.assetData = this.statmentPositionData['asset'];
+            this.liabilitiesData = this.statmentPositionData['liabilities'];
+        });
+    }
+
+    filterStatementOfPosition() {
+        if (this.filterStatementOfPositionReportForm.value.from === '' || this.filterStatementOfPositionReportForm.value.to === '') {
+            if (this.filterStatementOfPositionReportForm.value.from === '') {
+                this.alertService.showErrors("From date is required");
+            } else if (this.filterStatementOfPositionReportForm.value.to === '') {
+                this.alertService.showErrors("To date is required");
+            }
+        } else {
+            const f = this.filterStatementOfPositionReportForm.value;
+            const fromDate = moment(f.from).format('YYYY-MM-DD');
+            const toDate = moment(f.to).format('YYYY-MM-DD');
+            const params = {
+                fromDate: fromDate,
+                toDate: toDate
+            };
+            this.getStatementPositionData(params);
+        }
+    }
 }
