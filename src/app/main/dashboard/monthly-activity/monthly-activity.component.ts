a506eb539a682f8762aa2891dba30f79a8355c7f
anup.singh@luezoid.com <anup.singh@luezoid.com>
2020-11-03 18:16:45 +0530

    monthly-activity: WIP

--- src/app/main/dashboard/monthly-activity/monthly-activity.component.ts
+++ src/app/main/dashboard/monthly-activity/monthly-activity.component.ts
@@ -1,12 +1,13 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {fuseAnimations} from '@fuse/animations';
 import {JournalVoucherLedgerReportService} from 'app/shared/services/journal-voucher-ledger-report.service';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {EconomicSegmentSelectComponent} from "../journal-voucher/economic-segment-select/economic-segment-select.component";
-import {MatDialog} from "@angular/material/dialog";
-import * as moment from "moment";
-import {AlertService} from "../../../shared/services/alert.service";
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {EconomicSegmentSelectComponent} from '../journal-voucher/economic-segment-select/economic-segment-select.component';
+import {MatDialog} from '@angular/material/dialog';
+import * as moment from 'moment';
+import {AlertService} from '../../../shared/services/alert.service';
 import {MonthlyActivityEconomicSegmentComponent} from './monthly-activity-economic-segment/monthly-activity-economic-segment.component';
+import {MatAccordion} from '@angular/material/expansion';
 
 @Component({
     selector: 'app-monthly-activity',
@@ -149,6 +150,8 @@ export class MonthlyActivityComponent implements OnInit {
     dialogRef: any;
     filterJVLegderSiblingReportForm: FormGroup;
     economicSegments = [];
+    @ViewChild(MatAccordion) accordion: MatAccordion;
+    panelOpenState: boolean = false;
 
     constructor(private jvLedgerReportService: JournalVoucherLedgerReportService,
                 private fb: FormBuilder,
@@ -162,15 +165,17 @@ export class MonthlyActivityComponent implements OnInit {
         this.filterJVLegderSiblingReportForm = this.fb.group({
             'economicSegmentId': ['']
         });
+        // this.getChildData({id: 7}, 1);
     }
 
     getMonthlyActivityData(params) {
         this.jvLedgerReportService.getMonthlyActivityReport(params).subscribe(data => {
-            this.data = data.items
+            this.data = data.items;
         });
     }
 
-    getChildData(item, index) {
+    getChildData(item, index?) {
+        console.log('--->>>anup');
         const params = {};
         params['parentId'] = item.id;
         this.jvLedgerReportService.getMonthlyActivityReport(params).subscribe(data => {
@@ -201,5 +206,16 @@ export class MonthlyActivityComponent implements OnInit {
         const params = {};
         params['economicSegmentId'] = this.filterJVLegderSiblingReportForm.value.economicSegmentId;
         this.getMonthlyActivityData(params);
+    }
+
+    openAll() {
+        console.log('--->>>open all');
+        this.panelOpenState = !this.panelOpenState;
+        if (this.data && this.data.length) {
+            this.data.forEach(d => {
+                console.log('--->>data', d);
+                this.getChildData(d);
+            });
+        }
     }
 }
