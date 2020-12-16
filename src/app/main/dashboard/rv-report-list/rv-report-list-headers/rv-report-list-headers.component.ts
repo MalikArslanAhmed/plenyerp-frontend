1048b462b27592e65defdaca11809045222d6b53
anup.singh@luezoid.com <anup.singh@luezoid.com>
2020-12-16 16:08:13 +0530

    rv-report: add more columns

--- src/app/main/dashboard/rv-report-list/rv-report-list-headers/rv-report-list-headers.component.ts
+++ src/app/main/dashboard/rv-report-list/rv-report-list-headers/rv-report-list-headers.component.ts
@@ -13,9 +13,12 @@ export class RvReportListHeadersComponent implements OnInit {
     isSubmitted = false;
     // managers: any = [];
     updateData: any;
-
-    // availableColumns = ['SN', 'Title', 'File No', 'Staff ID', 'Gender', 'Marital Status', 'Emp. Photo', 'Expected Exit Date', 'Exited', 'First Name', 'Last Name', 'Grade Level', 'Type of Exit', 'Type of Appointment', 'TIN No', 'Street', 'State', 'PFA No', 'Salary Scale', 'RefNo TypeOfExit', 'RefNo TypeOfAppointment', 'Qualifications', 'Permanent Staff', 'Pension Started', 'New Enrollment', 'Passport No', 'Passport Issued on', 'Passport Issued at', 'Passport Expires on', 'NHF No', 'Nature Of Exit', 'Nationality', 'National ID No', 'Mobile Phone', 'Maiden Name', 'Job Position', 'Grade Level Step', 'Department', 'Address', 'Address Country', 'Email', 'Drivers Licence No', 'Designation', 'Date Last Increment', 'Date Pension Started', 'Date of Birth', 'Date Last Promoted', 'Date Current Appt', 'Date Employed', 'Date Assumed Duty', 'Confirmed', 'Address LGA', 'City', 'Address State', 'Citizen Country', 'Citizen LGA', 'Citizen Region', 'Citizen State', 'Confirmation Due Date'];
-    availableColumns = ['S.No.', 'Year', 'Deptal No.', 'Payee Names', 'Amount (Net)', 'Taxes', 'Payment Ref. (#)', 'Last Actioned', 'Status'];
+    availableColumns = ['S.No.', 'RV Year', 'Deptal No.', 'Payee Names', 'Amount (Net)', 'Payment Ref. (#)', 'Last Actioned', 'Currency',
+        'Value Date', 'Type Of Payee', 'Xrate', 'XRate To USD', 'Ref. AIE', 'Ref. AIE Title', 'Prepared Date', 'COA: Admin', 'COA: Programme', 'COA: Geo Code',
+        'COA: Fund', 'COA: Functional', 'COA: Economic', 'Admin FullCode', 'Admin: Title', 'Programme: Title', 'Programme: Full Code', 'Economic: Full Code',
+        'Economic: Title', 'Geo: Title', 'Geo: Full Code', 'Fund: Title', 'Fund: Full Code', 'Functional: Title', 'Functional: Full Code', 'Payee Id', 'Details/Naration', 'Account',
+        'Amount', 'Parent Voucher Year', 'Parent Voucher Ref', 'Paid TDate', 'Checked?', 'Approved?', 'Audited?',
+        'Approved Date', 'Checked Date', 'Audited Date', 'Paid?', 'Paid Date', 'Bank Acct ID', 'Approval RefNo', 'JV RefNo', 'Status'];
     selectedColumns = [];
 
 
