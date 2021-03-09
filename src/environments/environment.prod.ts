d8f256bcd70334ed23adc09920fb6b05f4b1252d
sahu <saurabh.sahu@luezoid.com>
2021-03-09 11:20:34 +0530

    dev build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     hmr       : false,
-    // appUrl: 'http://api.erp.valuescale.co.in/',
+    appUrl: 'http://api.erp.valuescale.co.in/',
     // appUrl: 'http://209.97.184.185/',
-    appUrl: 'http://178.62.58.157:8080/',
+    // appUrl: 'http://178.62.58.157:8080/',
 };
