b13e67980dd7cd057a56eda4b4c8f32ec3a18d8e
Saurabh <saurabh.sahu@luezoid.com>
2021-09-25 00:06:23 +0530

    build img

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: true,
     hmr       : false,
     // appUrl: 'http://api.erp.valuescale.co.in/',
-    appUrl: 'http://209.97.184.185/',
+    // appUrl: 'http://209.97.184.185/',
+    appUrl: 'http://178.62.58.157:8080/'
 };
