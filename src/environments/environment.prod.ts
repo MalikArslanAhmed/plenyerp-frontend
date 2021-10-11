4ac0fcc32d78070a78ad930ce2472626e4b288da
Saurabh <saurabh.sahu@luezoid.com>
2021-10-11 22:56:52 +0530

    tax object sending instead of taxid in payee v add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     hmr       : false,
     // appUrl: 'http://api.erp.valuescale.co.in/',
-    // appUrl: 'http://209.97.184.185/',
-    appUrl: 'http://178.62.58.157:8080/'
+     appUrl: 'http://209.97.184.185/',
+   // appUrl: 'http://178.62.58.157:8080/'
 };
