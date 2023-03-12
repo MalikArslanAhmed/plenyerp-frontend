aa2cdf73afcd7f02a44413c67fd2b5e2ee2a5f9b
Arslan Ahmed <malikzarslan44@gmail.com>
2023-03-12 22:10:17 +0500

    added printing

--- src/app/main/fixed-assets/reports/fixed-assets-depreciation-report/fixed-assets-depreciation-report.component.ts
+++ src/app/main/fixed-assets/reports/fixed-assets-depreciation-report/fixed-assets-depreciation-report.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { CompanyInformationService } from 'app/shared/services/company-information.service';
 import { FxaAssetsService } from 'app/shared/services/fxa-assets.service';
 import { FxaCategoriesService } from 'app/shared/services/fxa-categories.service';
+import { StatusService } from 'app/shared/services/status.service';
+import { WorkLocationService } from 'app/shared/services/work-location.service';
 import { FixedAssetsDepreciationReportModalComponent } from './fixed-assets-depreciation-report-modal/fixed-assets-depreciation-report-modal.component';
 
 @Component({
-  selector: 'app-fixed-assets-depreciation-report',
-  templateUrl: './fixed-assets-depreciation-report.component.html',
-  styleUrls: ['./fixed-assets-depreciation-report.component.scss']
+    selector: 'app-fixed-assets-depreciation-report',
+    templateUrl: './fixed-assets-depreciation-report.component.html',
+    styleUrls: ['./fixed-assets-depreciation-report.component.scss']
 })
 export class FixedAssetsDepreciaitonReportComponent implements OnInit {
-    displayedColumns = ['id', 'assetNo', 'title', 'acquistionCost','depreciation', 'accDepreciaition', 'netBookValue','remark'];
+    displayedColumns = ['id', 'assetNo', 'title', 'acquistionCost', 'depreciation', 'accDepreciaition', 'netBookValue', 'remark'];
 
     dialogRef: any;
     fixedAssetsReport = [];
@@ -23,50 +26,170 @@ export class FixedAssetsDepreciaitonReportComponent implements OnInit {
     };
     assetsForm: FormGroup;
     faCategories = '';
-    depMonth:number = null;
+    depMonth: number = null;
     fixedAssetId: any;
     fetching = false
     assetNo = ''
     location = ''
-    status= ''
+    status = ''
+    printData: any = [];
+    companyData: any = {}
+    statusData: any = []
+    workLocationsData: any = []
     constructor(
         private fxaCategoryService: FxaCategoriesService,
         private _matDialog: MatDialog,
         private fb: FormBuilder,
         private fxaAssetsService: FxaAssetsService,
-
+        private companyInformationService: CompanyInformationService,
+        private workLocationService: WorkLocationService
     ) {
     }
-printPage(){
-    window.print()
-}
+    printPage() {
+        this.getDataForPrint()
+    }
     ngOnInit(): void {
         this.refresh();
     }
     refresh(): void {
+        this.getCompanyInformation();
+        this.getStatus();
+        this.getWorkLocation();
+
         this.assetsForm = this.fb.group({
             fxaCategoryId: [''],
         });
     }
+    getWorkLocation() {
+        this.workLocationService.getWorkLocations({ page: null }).subscribe(data => {
+            this.workLocationsData = data;
+            console.log('work data data', this.workLocationsData);
+        });
+    }
+    getStatus() {
+        this.fxaCategoryService.getStatus({ page: null }).subscribe(data => {
+            this.statusData = data.items;
+            console.log('sattus data', this.statusData);
 
-    getFixedAssetReport(): void {
+        });
+    }
+    getCompanyInformation() {
+        this.companyInformationService.getCompaniesInformationList().subscribe(data => {
+            this.companyData = data.items[0];
+            console.log('companyData data', this.companyData);
+
+        });
+    }
+    getDataForPrint() {
         let params = {
             page: this.pagination.page,
             categoriesAllIds: this.faCategories,
             dep_month: this.depMonth,
-            location:this.location,
-            assetNo:this.assetNo,
-            status:this.status,
+            location: this.location,
+            assetNo: this.assetNo,
+            status: this.status,
+            print: true
         };
 
         this.fetching = true
         this.fxaAssetsService.fixedAssetsReport(params).subscribe(
             data => {
-                this.fixedAssetsReport = data.data.map(item=>{
-                    return {...item,
-                        acquisitionCost:+item['acquisitionCost'],
-                        beginAccumDepr:+item['beginAccumDepr'],
-                        currYrDepr:+item['currYrDepr'],
+                console.log(data);
+                this.printData = data.map(item => {
+                    return {
+                        ...item,
+                        acquisitionCost: +item['acquisitionCost'],
+                        beginAccumDepr: +item['beginAccumDepr'],
+                        icurrYrDepr: +item['currYrDepr'],
+                    }
+                })
+                this.print()
+            }
+        );
+    }
+    print() {
+        const WindowObject = window.open('', 'PrintWindow', 'width=750,height=650,top=50,left=50,toolbars=no,scrollbars=yes,status=no,resizable=yes'
+        );
+        const htmlData = `<html><head>
+             <style>
+            table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            }
+
+            td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+            }
+
+            tr:nth-child(even) {
+            background-color: #dddddd;
+            }
+            </style>
+        
+        </head><body>
+        <h1>Fixed Assets Report</h1>
+        <h1>${this.companyData.name}</h1>
+        <table>
+  <tr>
+    <th>S.No</th>
+    <th>Asset No</th>
+    <th>Asset Name</th>
+    <th>Acquisition Cost</th>
+    <th>Depreciation</th>
+    <th>Accumulated Depreciation</th>
+    <th>ANet Book Value</th>
+    <th>Remark</th>
+  </tr>
+${this.getAllRowsData()}
+</table>
+        </body></html>`;
+
+        WindowObject.document.writeln(htmlData);
+        WindowObject.document.close();
+        WindowObject.focus();
+        WindowObject.print()
+        setTimeout(() => {
+            WindowObject.close();
+        }, 0.5);
+    };
+    getAllRowsData() {
+        let rowDataString = ''
+        this.printData.forEach((item, index) => {
+            rowDataString += `<tr>
+            <td>${index + 1}</td>
+            <td>${item.assetNo}</td>
+            <td>${item.title}</td>
+            <td>${item.acquisitionCost}</td>
+            <td>${item.currYrDepr}</td>
+            <td>${item.beginAccumDepr + item.currYrDepr}</td>
+            <td>${item.acquisitionCost - (item.beginAccumDepr + item.currYrDepr)}</td>
+            <td>${item.remark}</td>
+            </tr>`
+        });
+        return rowDataString
+    }
+    getFixedAssetReport(): void {
+        let params = {
+            page: this.pagination.page,
+            categoriesAllIds: this.faCategories,
+            dep_month: this.depMonth,
+            location: this.location,
+            assetNo: this.assetNo,
+            status: this.status,
+        };
+
+        this.fetching = true
+        this.fxaAssetsService.fixedAssetsReport(params).subscribe(
+            data => {
+                this.fixedAssetsReport = data.data.map(item => {
+                    return {
+                        ...item,
+                        acquisitionCost: +item['acquisitionCost'],
+                        beginAccumDepr: +item['beginAccumDepr'],
+                        currYrDepr: +item['currYrDepr'],
                     }
                 });
                 this.pagination.page = data.currentPage;
@@ -96,11 +219,12 @@ printPage(){
             this.status = ''
             this.faCategories = response.categoriesAllIds
             this.depMonth = response.dep_month
-            this.fixedAssetsReport = response.data.data.map(item=>{
-                return {...item,
-                    acquisitionCost:+item['acquisitionCost'],
-                    beginAccumDepr:+item['beginAccumDepr'],
-                    currYrDepr:+item['currYrDepr'],
+            this.fixedAssetsReport = response.data.data.map(item => {
+                return {
+                    ...item,
+                    acquisitionCost: +item['acquisitionCost'],
+                    beginAccumDepr: +item['beginAccumDepr'],
+                    currYrDepr: +item['currYrDepr'],
                 }
             });
             this.pagination.page = response.data.currentPage;
--- src/app/main/fixed-assets/reports/fixed-assets-report/fixed-assets-report.component.ts
+++ src/app/main/fixed-assets/reports/fixed-assets-report/fixed-assets-report.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { CompanyInformationService } from 'app/shared/services/company-information.service';
 import { FxaAssetsService } from 'app/shared/services/fxa-assets.service';
 import { FxaCategoriesService } from 'app/shared/services/fxa-categories.service';
+import { WorkLocationService } from 'app/shared/services/work-location.service';
 import { FixedAssetsReportModalComponent } from './fixed-assets-report-modal/fixed-assets-report-modal.component';
 
 @Component({
@@ -29,26 +31,154 @@ export class FixedAssetsReportComponent implements OnInit {
     assetNo = ''
     location = ''
     status = ''
+    printData: any = [];
+    companyData: any = {}
+    statusData: any = []
+    workLocationsData: any = []
     constructor(
         private fxaCategoryService: FxaCategoriesService,
         private _matDialog: MatDialog,
         private fb: FormBuilder,
         private fxaAssetsService: FxaAssetsService,
+        private companyInformationService: CompanyInformationService,
+        private workLocationService: WorkLocationService
 
     ) {
     }
     printPage() {
-        window.print()
+        this.getDataForPrint()
     }
     ngOnInit(): void {
         this.refresh();
     }
     refresh(): void {
+        this.getCompanyInformation();
+        this.getStatus();
+        this.getWorkLocation();
         this.assetsForm = this.fb.group({
             fxaCategoryId: [''],
         });
     }
 
+    getWorkLocation() {
+        this.workLocationService.getWorkLocations({ page: null }).subscribe(data => {
+            this.workLocationsData = data;
+            console.log('work data data', this.workLocationsData);
+        });
+    }
+    getStatus() {
+        this.fxaCategoryService.getStatus({ page: null }).subscribe(data => {
+            this.statusData = data.items;
+            console.log('sattus data', this.statusData);
+
+        });
+    }
+    getCompanyInformation() {
+        this.companyInformationService.getCompaniesInformationList().subscribe(data => {
+            this.companyData = data.items[0];
+            console.log('companyData data', this.companyData);
+
+        });
+    }
+
+    getDataForPrint() {
+        let params = {
+            page: this.pagination.page,
+            categoriesAllIds: this.faCategories,
+            dep_month: this.depMonth,
+            location: this.location,
+            assetNo: this.assetNo,
+            status: this.status,
+            print: true
+        };
+
+        this.fetching = true
+        this.fxaAssetsService.fixedAssetsReport(params).subscribe(
+            data => {
+                console.log(data);
+                this.printData = data.map(item => {
+                    return {
+                        ...item,
+                        acquisitionCost: +item['acquisitionCost'],
+                        beginAccumDepr: +item['beginAccumDepr'],
+                        icurrYrDepr: +item['currYrDepr'],
+                    }
+                })
+                this.print()
+            }
+        );
+    }
+    print() {
+        const WindowObject = window.open('', 'PrintWindow', 'width=750,height=650,top=50,left=50,toolbars=no,scrollbars=yes,status=no,resizable=yes'
+        );
+        const htmlData = `<html><head>
+             <style>
+            table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            }
+
+            td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+            }
+
+            tr:nth-child(even) {
+            background-color: #dddddd;
+            }
+            </style>
+        
+        </head><body>
+        <h1>Fixed Assets Report</h1>
+        <table>
+  <tr>
+    <th>S.No</th>
+    <th>Asset No</th>
+    <th>Title</th>
+    <th>PV Year</th>
+    <th>PV Depart No</th>
+    <th>Current Location</th>
+    <th>Acquisition Date</th>
+    <th>Acquistion Cost</th>
+    <th>Depreciation To Date</th>
+    <th>Net Book Value</th>
+    <th>Make</th>
+    <th>Status</th>
+  </tr>
+${this.getAllRowsData()}
+</table>
+        </body></html>`;
+
+        WindowObject.document.writeln(htmlData);
+        WindowObject.document.close();
+        WindowObject.focus();
+        WindowObject.print()
+        setTimeout(() => {
+            WindowObject.close();
+        }, 0.5);
+    };
+    getAllRowsData() {
+        let rowDataString = ''
+        this.printData.forEach((item, index) => {
+            rowDataString += `<tr>
+            <td>${index + 1}</td>
+            <td>${item.assetNo}</td>
+            <td>${item.title}</td>
+            <td>${item.pvyear}</td>
+            <td>${item.pvDeptalNo}</td>
+            <td>${item.location}</td>
+            <td>${item.dateAcquired}</td>
+            <td>${item.acquisitionCost}</td>
+            <td>${item.beginAccumDepr + item.currYrDepr}</td>
+            <td>${item.acquisitionCost - (item.beginAccumDepr + item.currYrDepr)}</td>
+            <td>${item.make}</td>
+            <td>${item.status}</td>
+            </tr>`
+        });
+        return rowDataString
+    }
     getFixedAssetReport(): void {
         let params = {
             page: this.pagination.page,
@@ -67,7 +197,7 @@ export class FixedAssetsReportComponent implements OnInit {
                         ...item,
                         acquisitionCost: +item['acquisitionCost'],
                         beginAccumDepr: +item['beginAccumDepr'],
-                        icurrYrDepr: +item['currYrDepr'],
+                        currYrDepr: +item['currYrDepr'],
                     }
                 });
                 this.pagination.page = data.currentPage;
